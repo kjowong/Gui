bd7db66a6acd9492cff6c3777f99752c7e5d0e2f
Kimberly Wong <kjowong@gmail.com>
2017-10-11 11:46:30 +0000

    Initial commit

--- foursquare_2.py
+++ foursquare_2.py
@@ -5,8 +5,7 @@ from pymongo import MongoClient
 from geopy.geocoders import Nominatim
 import sys
 
-if __name__ == "__main__":
-
+def four_square():
     url = 'https://api.foursquare.com/v2/venues/explore'
     params = dict(
         client_id='ODAOUA2ZUGYKSAQMGSED4HVLAGQJZF4LU1FNEESGR2KY20CL',
@@ -14,17 +13,20 @@ if __name__ == "__main__":
         v='20171110',
         near = 'San Francisco, CA',
         section = 'food',
-        limit = 50
-)
+        limit = 3
+    )
 
     geolocator = Nominatim()
 
+    yelp_params = []
+
     r = requests.get(url=url, params=params).json()
-    fs_list = (r.get('response').get('groups')) 
+    fs_list = (r.get('response').get('groups'))
     for post in fs_list:
         posts = (post.get('items'))
         for place in posts:
-            print(place.get('venue').get('name'))
+            restaurant = {}
+            name = place.get('venue').get('name')
             street = place.get('venue').get('location').get('address')
             city = (place.get('venue').get('location').get('formattedAddress')[1]).replace(",", "")[:-5]
             address = street + " " +  city
@@ -32,8 +34,17 @@ if __name__ == "__main__":
                 location = geolocator.geocode(address)
 
                 if location is None:
-                    print(address)
+                    restaurant['name'] = name
+                    restaurant['location'] = address
+                    yelp_params.append(restaurant)
                 else:
-                    print(location)
+                    restaurant['name'] = name
+                    restaurant['location'] = str(location)
+                    yelp_params.append(restaurant)
             except Exception as e:
                 pass
+    return yelp_params
+
+if __name__ == "__main__":
+    four_square()
+

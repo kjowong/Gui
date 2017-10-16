e05a26771ea0a14f701ae879bc228fed6e39c4ce
spencerhcheng <spencerhcheng@gmail.com>
2017-10-16 14:20:26 +0000

    Implement bing map API for faster searching

--- foursquare_2.py
+++ foursquare_2.py
@@ -2,21 +2,21 @@
 
 import json, requests
 from pymongo import MongoClient
-from geopy.geocoders import Nominatim
+from geopy import geocoders
 import sys
 
 def four_square():
     url = 'https://api.foursquare.com/v2/venues/explore'
+    bing_key = "bNV4gwzuDF0BY7hRBr2D~SwYlwf_NvSxlbZ36oGsTdA~AthfnABkus6e2oSBb4W9Q9_7yHrFh1cHbreVFmsPad2apAgjYqLYZi8E2iSyiJk-"
+    bing = geocoders.Bing(bing_key)
     params = dict(
         client_id='ODAOUA2ZUGYKSAQMGSED4HVLAGQJZF4LU1FNEESGR2KY20CL',
         client_secret='TBGFJYKBYEUHORFJH3MCBB2DKP1SFBJ4ZUK2T3TVHGKBIHYD',
         v='20171110',
         near = 'San Francisco, CA',
         section = 'food',
-        limit = 3
+        limit = 50
     )
-
-    geolocator = Nominatim()
 
     yelp_params = []
 
@@ -31,15 +31,15 @@ def four_square():
             city = (place.get('venue').get('location').get('formattedAddress')[1]).replace(",", "")[:-5]
             address = street + " " +  city
             try:
-                location = geolocator.geocode(address)
-
+                for place in bing.geocode(address, exactly_one=False):
+                    location = ("%s\n" % (place))
                 if location is None:
                     restaurant['name'] = name
                     restaurant['location'] = address
                     yelp_params.append(restaurant)
                 else:
                     restaurant['name'] = name
-                    restaurant['location'] = str(location)
+                    restaurant['location'] = location
                     yelp_params.append(restaurant)
             except Exception as e:
                 pass
@@ -47,4 +47,3 @@ def four_square():
 
 if __name__ == "__main__":
     four_square()
-
